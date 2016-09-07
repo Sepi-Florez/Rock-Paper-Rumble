c27734abc426652ac7613beab971e62a8590aa67
David Vink <david.vink@hotmail.com>
2016-09-07 14:44:46 +0200

    TImer Progress
    Timer can now count down and restart with space. To do : make the
    countdown an int.

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMode : MonoBehaviour {
+	public void Selection (int selection) {
+		if(selection == 1){
+			Application.LoadLevel("Pvp");
+		}
+		else{
+			Application.LoadLevel("PvAI");
+		}
+	}
+}
---
+++ Assets/Scripts/PvAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PvAI : MonoBehaviour {
+	public Text countDown;
+	public Text timeUp;
+	public float timeLeft = 10f; 
+	public bool counting = false;
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if(counting == true){
+			Timer();
+		}
+		else{
+			if(Input.GetButtonDown("Jump")){
+				counting = true;
+				timeLeft = 10;
+				timeUp.gameObject.SetActive(false);
+			}
+		}
+	}
+	void Timer () {
+		timeLeft -= Time.deltaTime;
+		countDown.text = timeLeft.ToString();
+		if(timeLeft <= 0){
+			timeUp.gameObject.SetActive(true);
+			counting = false;
+		}
+	}
+}
