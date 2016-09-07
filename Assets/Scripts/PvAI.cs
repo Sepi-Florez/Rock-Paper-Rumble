667464626ca7fc30b4e0af9439380090f191c78f
David Vink <david.vink@hotmail.com>
2016-09-07 17:09:00 +0200

    Fixed timer with Ienumerator

--- Assets/Scripts/PvAI.cs
+++ Assets/Scripts/PvAI.cs
@@ -6,30 +6,39 @@ using System.Collections.Generic;
 public class PvAI : MonoBehaviour {
 	public Text countDown;
 	public Text timeUp;
-	public float timeLeft = 10f; 
+	public float timeLeft; 
+	public float timeCount;
+	public float waitTime = 1F;
 	public bool counting = false;
 	void Start () {
-	
+		
+		timeLeft = timeCount;
+		countDown.text = timeLeft.ToString("0");
 	}
 	
 	void Update () {
-		if(counting == true){
-			Timer();
-		}
-		else{
-			if(Input.GetButtonDown("Jump")){
-				counting = true;
-				timeLeft = 10;
-				timeUp.gameObject.SetActive(false);
-			}
+		if(Input.GetButtonDown("Jump")&& counting == false){	
+			StartCoroutine(Timer());
+			timeUp.gameObject.SetActive(false);
+			timeLeft = timeCount;
+			countDown.text = timeLeft.ToString("F0");
+			counting = true;	
 		}
 	}
-	void Timer () {
-		timeLeft -= Time.deltaTime;
-		countDown.text = timeLeft.ToString();
+	IEnumerator Timer () {
+		yield return new WaitForSeconds(waitTime);
+		timeLeft -= 1;
+		countDown.text = timeLeft.ToString("F0");
 		if(timeLeft <= 0){
 			timeUp.gameObject.SetActive(true);
 			counting = false;
+			StopCoroutine(Timer());
 		}
+		else{
+			StartCoroutine(Timer());
+		}	
+	}
+	void Pick () {
+		
 	}
 }
