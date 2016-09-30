aa29eb77823cf5cd8e5e26b2da123f24b02f4a41
David Vink <david.vink@hotmail.com>
2016-09-30 13:46:52 +0200

    PvAI Edits, Added choice and AI behaviour.

--- Assets/Scripts/PvAI.cs
+++ Assets/Scripts/PvAI.cs
@@ -10,7 +10,10 @@ public class PvAI : MonoBehaviour {
 	public float timeCount;
 	public float waitTime = 1F;
 	public bool counting = false;
-	void Start () {
+    public enum Choice {Rock, Paper, Scissors};
+    public Choice playerChoice;
+    public Choice aiChoice;
+    void Start () {
 		
 		timeLeft = timeCount;
 		countDown.text = timeLeft.ToString("0");
@@ -22,23 +25,46 @@ public class PvAI : MonoBehaviour {
 			timeUp.gameObject.SetActive(false);
 			timeLeft = timeCount;
 			countDown.text = timeLeft.ToString("F0");
-			counting = true;	
+			counting = true;
+            AIPick();
 		}
+        else if(counting == true) { 
+            Pick();
+        }
 	}
-	IEnumerator Timer () {
-		yield return new WaitForSeconds(waitTime);
-		timeLeft -= 1;
-		countDown.text = timeLeft.ToString("F0");
-		if(timeLeft <= 0){
-			timeUp.gameObject.SetActive(true);
-			counting = false;
-			StopCoroutine(Timer());
-		}
-		else{
-			StartCoroutine(Timer());
-		}	
-	}
-	void Pick () {
-		
-	}
+    IEnumerator Timer() {
+        yield return new WaitForSeconds(waitTime);
+        timeLeft -= 1;
+        countDown.text = timeLeft.ToString("F0");
+        if (timeLeft <= 0) {
+            timeUp.gameObject.SetActive(true);
+            counting = false;
+            StopCoroutine(Timer());
+        }
+        else {
+            StartCoroutine(Timer());
+        }
+    }
+    void Pick () {
+        switch (Input.inputString) {
+            case "q":
+                print("Rock");
+                playerChoice = (Choice)0;
+                break;
+
+            case "w":
+                print("Paper");
+                playerChoice = (Choice)1;
+                break;
+
+            case "e":
+                print("Scissors");
+                playerChoice = (Choice)2;
+                break;
+        }
+    }
+    void AIPick() {
+        aiChoice = (Choice)Random.Range(0, 3);
+    }
+
 }
